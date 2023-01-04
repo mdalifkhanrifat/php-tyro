3b32ccb8769074655038b6948cec779ae125ece9
mdalifkhanrifat <mdalifkhanrifat@gmail.com>
2023-01-04 21:09:09 +0600

    Update task-8

--- task-8/gpaCalculator.php
+++ task-8/gpaCalculator.php
@@ -7,270 +7,315 @@ if (isset($_GET['operation'])) {
     $operation = 'null';
 }
 
-if (isset($_GET['operation2'])) {
-    $operation2 = $_GET['operation2'];
+if (isset($_GET['cgpa'])) {
+    $cgpa = $_GET['cgpa'];
 } else {
-    $operation2 = 'null';
+    $cgpa = 0.0;
 }
 
 
-if ($operation2==="Scienc"){
-    if (isset($_GET['Physics'])) {
-        $Physics = $_GET['Physics'];
-    } else {
-        $Physics = 'null';
-    }
-    if (isset($_GET['Chemistry'])) {
-        $Chemistry = $_GET['Chemistry'];
-    } else {
-        $Chemistry = '010';
-    }
-    if (isset($_GET['Biology'])) {
-        $Biology = $_GET['Biology'];
-    } else {
-        $Biology = '010';
-    }
-    $point = array("Physics"=> convertGpa($Physics, $operation), "Chemistry"=> convertGpa($Chemistry, $operation), "Biology"=> convertGpa($Biology, $operation));
-    $grade = array("Physics"=> convertGpa($Physics, $operation), "Chemistry"=> convertGpa($Chemistry, $operation), "Biology"=> convertGpa($Biology, $operation));
-    
+// if ($operation2 === "Scienc") {
+//     if (isset($_GET['Physics'])) {
+//         $Physics = $_GET['Physics'];
+//     } else {
+//         $Physics = 'null';
+//     }
+//     if (isset($_GET['Chemistry'])) {
+//         $Chemistry = $_GET['Chemistry'];
+//     } else {
+//         $Chemistry = '010';
+//     }
+//     if (isset($_GET['Biology'])) {
+//         $Biology = $_GET['Biology'];
+//     } else {
+//         $Biology = '010';
+//     }
+// }
 
 
-}
+// $point = ["Physics"=> convertGpa($Physics, $operation), "Chemistry"=> convertGpa($Chemistry, $operation), "Biology"=> convertGpa($Biology, $operation)];
+$grade = convertgrade($cgpa, $operation);
 
-function convertGpa($gpa, $level)
+
+
+// function convertGpa($gpa, $level)
+// {
+//     if($gpa >= 80 && $gpa >=100)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return 5.0;
+//         }
+//         else if($level === 'Graduation' || $level === 'Masters' )
+//         {
+//             return 4.0;
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+
+//     else if($gpa >= 70 && $gpa >=79)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return 4.0;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=70 && $gpa>=74))
+//         {
+//             return 3.50;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=75 && $gpa>=79) )
+//         {
+//             return 3.75;
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+
+//     else if($gpa >= 60 && $gpa >=69)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return 3.50;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=60 && $gpa>=64))
+//         {
+//             return 3.0;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=65 && $gpa>=69) )
+//         {
+//             return 3.25;
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+//     else if($gpa >= 50 && $gpa >=59)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return 3.0;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=50 && $gpa>=54))
+//         {
+//             return 2.50;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=55 && $gpa>=59) )
+//         {
+//             return 2.75;
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+
+//      else if($gpa >= 40 && $gpa >=49)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return 2.0;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=40 && $gpa>=44))
+//         {
+//             return 2.0;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=45 && $gpa>=49) )
+//         {
+//             return 2.25;
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+
+//     else if($gpa >= 33 && $gpa >=39)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return 1.0;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters'))
+//         {
+//             return 0.0;
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+//     else{
+//         return 0.0;
+//     }
+// }
+
+
+// function grade($gpa, $level)
+// {
+//     if($gpa >= 3.0 && $gpa >=4)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return "A+";
+//         }
+//         else if($level === 'Graduation' || $level === 'Masters' )
+//         {
+//             return "A+";
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+
+//     else if($gpa >= 70 && $gpa >=79)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return "A";
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=70 && $gpa>=74))
+//         {
+//             return "A-";
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=75 && $gpa>=79) )
+//         {
+//             return "A";
+//         }
+//         else
+//         {
+//             return "N";
+//         }
+//     }
+
+//     else if($gpa >= 60 && $gpa >=69)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return "A-";
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=60 && $gpa>=64))
+//         {
+//             return "B+";
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=65 && $gpa>=69) )
+//         {
+//             return "A-";
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+//     else if($gpa >= 50 && $gpa >=59)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return "B";
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=50 && $gpa>=54))
+//         {
+//             return "C+";
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=55 && $gpa>=59) )
+//         {
+//             return "B-";
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+
+//     else if($gpa >= 40 && $gpa >=49)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return "C";
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=40 && $gpa>=44))
+//         {
+//             return "D";
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=45 && $gpa>=49) )
+//         {
+//             return "C";
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+
+//     else if($gpa >= 33 && $gpa >=39)
+//     {
+//         if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
+//         {
+//             return 1.0;
+//         }
+//         else if(($level === 'Graduation' || $level === 'Masters'))
+//         {
+//             return "F";
+//         }
+//         else
+//         {
+//             return "N";
+//         }
+//     }
+//     else{
+//         return "F";
+//     }
+// }
+
+function convertgrade(float $gpa, $level)
 {
-    if($gpa >= 80 && $gpa >=100)
+    if ($level === "HSC" || $level === "JSC" || $level === "SSC") 
     {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
-            return 5.0;
-        }
-        else if($level === 'Graduation' || $level === 'Masters' )
-        {
-            return 4.0;
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-
-    else if($gpa >= 70 && $gpa >=79)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
-            return 4.0;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=70 && $gpa>=74))
-        {
-            return 3.50;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=75 && $gpa>=79) )
-        {
-            return 3.75;
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-
-    else if($gpa >= 60 && $gpa >=69)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
-            return 3.50;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=60 && $gpa>=64))
-        {
-            return 3.0;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=65 && $gpa>=69) )
-        {
-            return 3.25;
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-    else if($gpa >= 50 && $gpa >=59)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
-            return 3.0;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=50 && $gpa>=54))
-        {
-            return 2.50;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=55 && $gpa>=59) )
-        {
-            return 2.75;
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-
-     else if($gpa >= 40 && $gpa >=49)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
-            return 2.0;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=40 && $gpa>=44))
-        {
-            return 2.0;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=45 && $gpa>=49) )
-        {
-            return 2.25;
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-
-    else if($gpa >= 33 && $gpa >=39)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
-            return 1.0;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters'))
-        {
-            return 0.0;
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-    else{
-        return 0.0;
-    }
-}
-
-
-function grade($gpa, $level)
-{
-    if($gpa >= 80 && $gpa >=100)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
-            return "A+";
-        }
-        else if($level === 'Graduation' || $level === 'Masters' )
-        {
-            return "A+";
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-
-    else if($gpa >= 70 && $gpa >=79)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
+        if ($gpa == 5.0) {
+            return 'A+';
+        } else if ($gpa >= 4.0 && $gpa <= 4.9) {
             return "A";
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=70 && $gpa>=74))
-        {
+        } else if ($gpa >= 3.5 && $gpa <= 3.9) {
             return "A-";
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=75 && $gpa>=79) )
-        {
-            return "A";
-        }
-        else
-        {
-            return "N";
-        }
-    }
-
-    else if($gpa >= 60 && $gpa >=69)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
-            return "A-";
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=60 && $gpa>=64))
-        {
-            return "B+";
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=65 && $gpa>=69) )
-        {
-            return "A-";
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-    else if($gpa >= 50 && $gpa >=59)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
+        } else if ($gpa >= 3.0 && $gpa <= 3.4) {
             return "B";
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=50 && $gpa>=54))
-        {
-            return "C+";
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=55 && $gpa>=59) )
-        {
-            return "B-";
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-
-    else if($gpa >= 40 && $gpa >=49)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
+        } else if ($gpa >= 2.0 && $gpa <= 2.9) {
             return "C";
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=40 && $gpa>=44))
-        {
+        } else if ($gpa >= 1.0 && $gpa <= 1.9) {
             return "D";
-        }
-        else if(($level === 'Graduation' || $level === 'Masters') && ($gpa>=45 && $gpa>=49) )
-        {
-            return "C";
-        }
-        else
-        {
-            return 0.0;
-        }
-    }
-
-    else if($gpa >= 33 && $gpa >=39)
-    {
-        if($level === 'JSC' || $level === 'HSC' || $level === 'SSC')
-        {
-            return 1.0;
-        }
-        else if(($level === 'Graduation' || $level === 'Masters'))
-        {
+        } else {
             return "F";
         }
-        else
-        {
-            return "N";
+    }
+
+    else
+    {
+        if ($gpa == 4.0) {
+            return 'A+';
+        } else if ($gpa >= 3.75 && $gpa <= 3.9) {
+            return "A";
+        } else if ($gpa >= 3.50 && $gpa <= 3.74) {
+            return "A-";
+        } else if ($gpa >= 3.25 && $gpa <= 3.49) {
+            return "B+";
+        } else if ($gpa >= 3.0 && $gpa <= 3.24) {
+            return "B";
+        }  else if ($gpa >= 2.75 && $gpa <= 2.9) {
+            return "B-";
+        } else if ($gpa >= 2.50 && $gpa <= 2.74) {
+            return "C+";
+        } else if ($gpa >= 2.25 && $gpa <= 3.49) {
+            return "B";
+        }else if ($gpa >= 2.0 && $gpa <= 2.24) {
+            return "B";
+        }else {
+            return "F";
         }
     }
-    else{
-        return "F";
-    }
 }
-
-
-
--- task-8/index.php
+++ task-8/index.php
@@ -33,116 +33,21 @@ include_once 'gpaCalculator.php';
                         <h1>Masters</h1>
                     </option>
                 </select>
+                <label for="CGPA">Physics:</label>
+            <input class="#" name="cgpa" type="number" step="0.1" placeholder="CGPA" width="48" height="48">
                 <button class="input_box" type="submit" width="48" height="48"> = </button>
             </form>
         </section>
         <section>
-            <?php
 
-
-
-
-
-            if ($operation == "SSC" || $operation == "HSC") {
-                echo '<section><form action="" method="GET">
-               <select name="operation2" id="#">
-                   <option value="Science">
-                       <h1>Science</h1>
-                   </option>
-                   <option value="Arts">
-                       <h1>Arts</h1>
-                   </option>
-                   <option value="Commerce">
-                       <h1>Commerce</h1>
-                   </option>
-               </select>
-               <button class="input_box" type="submit" width="48" height="48"> = </button>
-           </form> </section>';
-            }
-
-
-
-            if ($operation2 === "Science") {
-                echo 'Science' . PHP_EOL;
-                echo '<form action="" method="GET">
-                <label for="Physics">Physics:</label>
-            <input class="#" name="Physics" type="number" step="0.1" placeholder="marks" width="48" height="48">
-            <label for="Chemistry">Chemistry:</label>
-            <input class="#" name="Chemistry" type="number" step="0.1" placeholder="marks" width="48" height="48">
-            <label for="Biology">Biology:</label>
-            <input class="#" name="Biology" type="number" step="0.1" placeholder="marks" width="48" height="48">
-            <button class="input_box" type="submit" width="48" height="48"> = </button>
-            </form>
-            ';
-            } else if ($operation2 === "Commerce") {
-                echo 'commerce' . PHP_EOL;
-                echo '<form action="" method="GET">
-            <input class="#" name="Finance" type="text" placeholder="Bangla" width="48" height="48">
-            <input class="#" name="Accounting" type="text" placeholder="English" width="48" height="48">
-            <input class="#" name="Business" type="text" placeholder="Math" width="48" height="48">
-            <button class="input_box" type="submit" width="48" height="48"> = </button>
-            </form>
-            ';
-            } else if ($operation2 === "Arts") {
-                echo 'Arts' . PHP_EOL;
-                echo '<form action="" method="GET">
-            <input class="#" name="Geography" type="text" placeholder="Bangla" width="48" height="48">
-            <input class="#" name="Civic" type="text" placeholder="English" width="48" height="48">
-            <input class="#" name="History" type="text" placeholder="Math" width="48" height="48">
-            <button class="input_box" type="submit" width="48" height="48"> = </button>
-            </form>
-            ';
-            }
-
-            if ($operation === "Graduation") {
-                echo 'Graduation' . PHP_EOL;
-                echo ' <form action="" method="GET">
-                <input class="#" name="sub1" type="text" placeholder="Bangla" width="48" height="48">
-                <input class="#" name="sub2" type="text" placeholder="English" width="48" height="48">
-                <input class="#" name="sub3" type="text" placeholder="Math" width="48" height="48">
-                <button class="input_box" type="submit" width="48" height="48"> = </button>
-                </form>
-                    ';
-            }
-
-            if ($operation === "Masters") {
-                echo 'Masters' . PHP_EOL;
-                echo ' <form action="" method="GET">
-                <input class="#" name="bangla" type="text" placeholder="Bangla" width="48" height="48">
-                <button class="input_box" type="submit" width="48" height="48"> = </button>
-                </form>
-                    ';
-            }
-
-
-            if ($operation === "JSC") {
-                echo 'jSC' . PHP_EOL;
-                echo '<form action="" method="GET">
-                <input class="#" name="Bangla" type="text" placeholder="Marks" width="48" height="48">
-                <input class="#" name="English" type="text" placeholder="Marks" width="48" height="48">
-                <input class="#" name="Mathematics" type="text" placeholder="Marks" width="48" height="48">
-                <input class="#" name="Science" type="text" placeholder="Marks" width="48" height="48">
-                <input class="#" name="Religions" type="text" placeholder="Marks" width="48" height="48">
-                <input class="#" name="Bangladesh_Global" type="text" placeholder="Marks" width="48" height="48">
-                <input class="#" name="Information_Technology" type="text" placeholder="Marks" width="48" height="48">
-                <button class="input_box" type="submit" width="48" height="48"> = </button>
-                </form>
-                    ';
-            }
-
-            if($Chemistry >= 1)
-            {
-                
-                echo "Hello";
-                echo $grade['Chemistry'];
-            }
-
-            ?>
-        </section>
         <?php
-        
+
+        echo 'Result: '.$grade;
 
         ?>
+            
+        </section>
+      
 
     </section>
 
