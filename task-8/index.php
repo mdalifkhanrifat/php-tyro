b235dbb637f8530554644f269a5ce03f50725f2b
mdalifkhanrifat <mdalifkhanrifat@gmail.com>
2023-01-05 03:24:22 +0600

    Update task-8

--- task-8/index.php
+++ task-8/index.php
@@ -11,38 +11,64 @@ include_once 'gpaCalculator.php';
     <title>Document</title>
 
     <style>
-        <style>
-input[type=text], select {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+        input[type=number],
+        select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
 
-input[type=submit] {
-  width: 100%;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
+        input[type=submit] {
+            width: 100%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
 
-input[type=submit]:hover {
-  background-color: #45a049;
-}
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
 
-section {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
-</style>
+        section {
+            border-radius: 5px;
+            background-color: #f2f2f2;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .output {
+
+            background-color: #77BFC7;
+            color: white;
+            text-align: center;
+
+        }
+
+        .res {
+            background-color: #77BFC7;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            text-align: center;
+            top: 50%;
+            -ms-transform: translateY(-50%);
+            transform: translateY(-50%);
+        }
+    </style>
     </style>
 </head>
 
@@ -69,21 +95,19 @@ section {
                     </option>
                 </select>
                 <label for="gpa">CGPA:</label>
-            <input class="#" name="gpa" type="number" step="0.1" placeholder="CGPA">
-                <button class="input_box" type="submit"> = </button>
+                <input class="" name="gpa" type="number" step="0.1" placeholder="CGPA">
+                <input class="" type="submit">
             </form>
         </section>
         <section>
-
-        <?php
-
-
-        echo 'Result: '.$grade;
-
-        ?>
-            
+            <a class="res">Result</a>
         </section>
-      
+        <section class="output">
+
+            <?php echo '<b>' . $grade . '</b>'; ?>
+
+        </section>
+
 
     </section>
 
