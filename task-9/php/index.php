5261cd2db0cd050edb5039ea67295c93f0b9da19
mdalifkhanrifat <mdalifkhanrifat@gmail.com>
2023-01-17 13:20:20 +0600

    update task-9

--- task-9/php/index.php
+++ task-9/php/index.php
@@ -11,9 +11,9 @@
 
 <body class="bg-gray-400 py-2 px-11">
     <header>
-        <section class="h-40 bg-green-300 ">
-            <h1>Banner Section</h1>
-        </section>
+    
+
+        <img class="h-40 w-full" src="../img/bb.jpg" alt="" >
 
         <section>
             <nav class="h-8 bg-green-500 flex p-1/20 justify-between items-center text-right flex-1">
@@ -29,8 +29,8 @@
         </section>
     </header>
     <section class="relative">
-        <section class="absolut inset-y-0 left-0 w-8/12">
-            <section class="container m-auto grid grid-cols-2 inset-y-0 left-0">
+        <section class="absolut inset-y-0 left-0 w-8/12  bg-slate-300">
+            <section class="container m-auto grid grid-cols-2 inset-y-0 left-0 bg-slate-300">
                 <section class="tile flex-none m-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
@@ -58,6 +58,36 @@
                         Read more
                     </a>
                 </section>
+                <section class="tile flex-none m-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Read more
+                    </a>
+                </section>
+
+                <section class="tile flex-none m-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Read more
+                    </a>
+                </section>
+
+                <section class="tile flex-none m-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Read more
+                    </a>
+                </section>
+
                 <section class="tile flex-none m-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
@@ -68,25 +98,99 @@
                     </a>
                 </section>
             </section>
+            <a href="#" type="submit" class="text-right">Next</a>
         </section>
-        <section class="absolute inset-y-0 right-0 w-1/3">
-            <section class="tile flex-none m-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+
+        <section class="absolute inset-y-0 right-0 w-1/3 bg-white">
+            <h1 class="flex-none text-center max-h-40"> Live News</h1>
+            <section class="tile flex-none w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                </a>
             </section>
 
+            <section class="tile flex-none w-full max-w-md p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <iframe src="https://www.youtube.com/embed/IXDWdpWA5MU"></iframe>
+            </section>
+
+            <section class="w-full max-w-md p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <section class="flex items-center justify-between mb-4">
+                    <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Author</h5>
+                    <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">View all</a>
+                </section>
+
+                <section class="flow-root">
+                    <ul role="list" class="sectionide-y sectionide-gray-200 dark:sectionide-gray-700">
+                        <li class="py-3 sm:py-4">
+                            <section class="flex items-center space-x-4">
+                                <section class="flex-shrink-0">
+                                    <img class="w-8 h-8 rounded-full" src="../img/sojol.jpg" alt="Neil image">
+                                </section>
+                                <section class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">Faruk Ahmed Sojol</p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">farukahmedsojol@gmail.com</p>
+                                </section>
+                            </section>
+                        </li>
+                        <li class="py-3 sm:py-4">
+                            <section class="flex items-center space-x-4">
+                                <section class="flex-shrink-0">
+                                    <img class="w-8 h-8 rounded-full" src="../img/alif.jpg" alt="Bonnie image">
+                                </section>
+                                <section class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">Md Alif Khan Rifat</p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">mdalifkhanrifat@gmail.com</p>
+                                </section>
+                            </section>
+                        </li>
+                        <li class="py-3 sm:py-4">
+                            <section class="flex items-center space-x-4">
+                                <section class="flex-shrink-0">
+                                    <img class="w-8 h-8 rounded-full" src="../img/ponik.jpg" alt="Michael image">
+                                </section>
+                                <section class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white"> Islam Ponik</p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">samiul@gmail.com</p>
+                                </section>
+
+                            </section>
+                        </li>
+                        <li class="py-3 sm:py-4">
+                            <section class="flex items-center space-x-4">
+                                <section class="flex-shrink-0">
+                                    <img class="w-8 h-8 rounded-full" src="../img/pulock.jpg" alt="Lana image">
+                                </section>
+                                <section class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">Mrk Pulock</p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400"> mrkpulock@gmail.com</p>
+                                </section>
+                            </section>
+                        </li>
+                        <li class="pt-3 pb-0 sm:pt-4">
+                            <section class="flex items-center space-x-4">
+                                <section class="flex-shrink-0">
+                                    <img class="w-8 h-8 rounded-full" src="../img/nirob.jpg" alt="Thomas image">
+                                </section>
+                                <section class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">Nirob</p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">nirob@nirob.com</p>
+                                </section>
+                            </section>
+                        </li>
+                    </ul>
+                </section>
+            </section>
         </section>
-
     </section>
-    <footer class="h-40 bg-stone-900 items-center text-center">
-        <h1 class="text-white">Copyright © Md Alif Kahn Rifat,2023</h1>
+    <footer class="h-40 bg-stone-900 ">
+        <ul class="items-start">
+            <li><a href="#" class="text-white ">Facebook</a></li>
+            <li><a href="#" class="text-white ">Twitter</a></li>
+            <li><a href="#" class="text-white ">WhatsApp</a></li>
+        </ul>
+        <h1 class="text-white items-center text-center">Copyright © Md Alif Kahn Rifat,2023</h1>
     </footer>
-
 </body>
 
 </html>
